50729b6d1a56e5525e2e6cd5f0a72a6685a86dec
toyama satoshi <toyamarinyon@gmail.com>
2014-12-18 10:59:59 +0000

    Update

--- app/dest/assets/script/app.js
+++ app/dest/assets/script/app.js
@@ -9,7 +9,9 @@ PLAYER_WIDTH = 370;
 PLAYER_HEIGHT = 320;
 
 ASSETS = {
-  "player": "assets/image/player.png"
+  "player": "assets/image/player.png",
+  "back": "assets/image/background.png",
+  "ground": "assets/image/ground.png"
 };
 
 tm.main(function() {
@@ -25,6 +27,7 @@ tm.main(function() {
     height: SCREEN_HEIGHT
   });
   app.replaceScene(loadingScene);
+  app.enableController = false;
   return app.run();
 });
 
@@ -46,11 +49,32 @@ tm.define("MainScene", {
   superClass: "tm.app.Scene",
   init: function() {
     this.superInit();
+    this.worldSpeed = 0;
+    this.back1 = tm.app.Sprite("back", SCREEN_WIDTH, SCREEN_HEIGHT).addChildTo(this);
+    this.back1.position.set(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
+    this.back2 = tm.app.Sprite("back", SCREEN_WIDTH, SCREEN_HEIGHT).addChildTo(this);
+    this.back2.position.set(SCREEN_WIDTH / 2, -SCREEN_HEIGHT / 2);
+    this.ground = tm.app.Sprite("ground", SCREEN_WIDTH, SCREEN_HEIGHT).addChildTo(this);
+    this.ground.position.set(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
     this.enta = Enta().addChildTo(this);
-    this.enta.position.set(SCREEN_WIDTH / 2, SCREEN_HEIGHT);
-    return this.addEventListener("pointingend", function(event) {
-      return event.app.replaceScene(EndScene());
-    });
+    return this.enta.position.set(SCREEN_WIDTH / 2, SCREEN_HEIGHT - PLAYER_HEIGHT / 1.4 - 170);
+  },
+  update: function(app) {
+    if (this.worldSpeed < 5) {
+      this.worldSpeed += 0.05;
+    }
+    this.ground.y += this.worldSpeed;
+    this.back1.y += this.worldSpeed;
+    this.back2.y += this.worldSpeed;
+    if (this.back1.y > SCREEN_HEIGHT + SCREEN_HEIGHT / 2) {
+      this.back1.y = -(SCREEN_HEIGHT - this.back2.y);
+    }
+    if (this.back2.y > SCREEN_HEIGHT + SCREEN_HEIGHT / 2) {
+      this.back2.y = -(SCREEN_HEIGHT - this.back1.y);
+    }
+    if (this.worldSpeed > 2.0) {
+      return app.enableController = true;
+    }
   }
 });
 
@@ -71,7 +95,42 @@ tm.define("EndScene", {
 
 tm.define("Enta", {
   superClass: "tm.app.Sprite",
+  direction: 'left',
+  degree: 90,
   init: function() {
-    return this.superInit("player", PLAYER_WIDTH / 2, PLAYER_HEIGHT / 2);
+    this.superInit("player", PLAYER_WIDTH / 1.4, PLAYER_HEIGHT / 1.4);
+    return this.origin.y = 0;
+  },
+  update: function(app) {
+    var accel;
+    if (app.pointing.getPointingStart()) {
+      this.direction = this.direction === 'left' ? 'right' : 'left';
+    }
+    if (app.enableController) {
+      if (this.direction === 'left') {
+        if (this.degree > 45) {
+          this.degree -= 1;
+        }
+      } else {
+        if (this.degree < 135) {
+          this.degree += 1;
+        }
+      }
+    }
+    accel = Math.cos(this.degree * (Math.PI / 180));
+    this.x += accel * 20.0;
+    this.rotation = -(this.degree - 90);
+    if (this.rotation < -35) {
+      this.rotation = -35;
+    }
+    if (this.rotation > 35) {
+      this.rotation = 35;
+    }
+    if (this.x > SCREEN_WIDTH) {
+      this.x = SCREEN_WIDTH;
+    }
+    if (this.x < 0) {
+      return this.x = 0;
+    }
   }
 });
--- app/src/assets/script/app.coffee
+++ app/src/assets/script/app.coffee
@@ -7,6 +7,8 @@ PLAYER_HEIGHT = 320
 
 ASSETS = 
   "player": "assets/image/player.png"
+  "back": "assets/image/background.png"
+  "ground": "assets/image/ground.png"
 
 tm.main ->
   app = tm.display.CanvasApp "#World"
@@ -21,6 +23,8 @@ tm.main ->
     height: SCREEN_HEIGHT
 
   app.replaceScene loadingScene
+
+  app.enableController = false
 
   app.run()
 
@@ -39,10 +43,28 @@ tm.define "MainScene",
   superClass: "tm.app.Scene"
   init: ->
     this.superInit()
+    this.worldSpeed = 0
+    this.back1 = tm.app.Sprite("back", SCREEN_WIDTH, SCREEN_HEIGHT).addChildTo this
+    this.back1.position.set SCREEN_WIDTH/2, SCREEN_HEIGHT/2
+    this.back2 = tm.app.Sprite("back", SCREEN_WIDTH, SCREEN_HEIGHT).addChildTo this
+    this.back2.position.set SCREEN_WIDTH/2, -SCREEN_HEIGHT/2
+    this.ground = tm.app.Sprite("ground", SCREEN_WIDTH, SCREEN_HEIGHT).addChildTo this
+    this.ground.position.set SCREEN_WIDTH/2, SCREEN_HEIGHT/2
     this.enta = Enta().addChildTo this
-    this.enta.position.set SCREEN_WIDTH/2, SCREEN_HEIGHT
-    this.addEventListener "pointingend", (event) ->
-      event.app.replaceScene EndScene()
+    this.enta.position.set SCREEN_WIDTH/2, SCREEN_HEIGHT - PLAYER_HEIGHT/1.4 -170
+    # this.addEventListener "pointingend", (event) ->
+    #   event.app.replaceScene EndScene()
+  update: (app) ->
+    this.worldSpeed+= 0.05 if this.worldSpeed < 5
+    this.ground.y += this.worldSpeed
+    this.back1.y += this.worldSpeed
+    this.back2.y += this.worldSpeed
+    if this.back1.y > SCREEN_HEIGHT + SCREEN_HEIGHT/2
+      this.back1.y = -(SCREEN_HEIGHT-this.back2.y)
+    if this.back2.y > SCREEN_HEIGHT + SCREEN_HEIGHT/2
+      this.back2.y = -(SCREEN_HEIGHT-this.back1.y)
+    if this.worldSpeed > 2.0
+      app.enableController = true
 
 tm.define "EndScene",
   superClass: "tm.app.ResultScene"
@@ -58,5 +80,35 @@ tm.define "EndScene",
 
 tm.define "Enta",
   superClass: "tm.app.Sprite"
+  direction: 'left'
+  degree: 90
   init: ->
-    this.superInit "player", PLAYER_WIDTH/2, PLAYER_HEIGHT/2
+    this.superInit "player", PLAYER_WIDTH/1.4, PLAYER_HEIGHT/1.4
+    this.origin.y = 0
+
+  update: (app) ->
+    if app.pointing.getPointingStart()
+      this.direction = if this.direction is 'left' then 'right' else 'left'
+
+    if app.enableController
+      if this.direction is 'left'
+        this.degree-= 1 if this.degree > 45
+      else
+        this.degree+= 1 if this.degree < 135
+
+    accel = Math.cos this.degree * (Math.PI/180)
+
+    this.x += accel * 20.0
+
+    this.rotation = -(this.degree - 90)
+
+    if this.rotation < -35
+      this.rotation = -35
+    if this.rotation > 35
+      this.rotation = 35
+
+    if this.x > SCREEN_WIDTH
+      this.x = SCREEN_WIDTH
+    if this.x < 0
+      this.x = 0
+
